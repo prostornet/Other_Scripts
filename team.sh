7393a4c8f5094ef86c46f0b59c2a6b486ceeb3f6
Sergey B <32869683+prostornet@users.noreply.github.com>
2018-11-13 10:26:13 +0300

    Update team.sh

--- team.sh
+++ team.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-TMPF1=$(mktemp)
 FDEVTMP=$(mktemp)
 FDEVTEAM=/etc/sysconfig/network-scripts/ifcfg-team0
-FDEVBOND=/etc/sysconfig/network-scripts/ifcfg-team0
+FDEVBOND=/etc/sysconfig/network-scripts/ifcfg-bond0
 
 ip a | grep -B 1 'link/ether' | grep -e '^[1-9]' | awk -F':' '{print $2}' >> ${FDEVTMP}
 
@@ -19,7 +18,7 @@ done < ${FDEVTMP}
 cr_bd_ports() {
 count=1
 while read line; do
-    nmcli con add type team-slave con-name port${count} ifname ${line} master team0
+    nmcli con add type bond-slave con-name port${count} ifname ${line} master bond0
     nmcli con del ${line} > /dev/null 2>&1
     count=$(( count+=1 ))
 done < ${FDEVTMP}
@@ -30,42 +29,43 @@ read -p "Select mode Active-Backup(AB) or LACP(LA): " MOD
 read -p "Set ip addres with mask prefix: " IPADDR
 read -p "Set gw addres: " GWADDR
 
-if [ -e ${FDEVTEAM}]; then exit 0; fi
-if [ -e ${FDEVBOND}]; then exit 0; fi
+if [ -e ${FDEVTEAM} ]; then exit 0; fi
+if [ -e ${FDEVBOND} ]; then exit 0; fi
 
-if [[ ${TYP} -eq "B" ]]; then
-  if [[ ${MOD} -eq "AB" ]]; then
+if [ ${TYP} = "B" ]; then
+  if [ ${MOD} = "AB" ]; then
     echo "Bonding in Active-Backup mode is deprecated, we are going to build Team Active-Backup"
-    nmcli con add type team con-name team0 ifname team0 ip4 ${IPADDDR} GW4 ${GWADDR} ipv4.dns 'dns_addr1 dns_addr2 dns_addr3' ipv4.method manual ipv6.method ignore
-    echo "TEAM_CONFIG='{\"funner\":{\"name\":\"activebackup\"},\"link_watch\":{\"name\":\"ethtool\"}}'" >> ${FDEVTEAM}
+    nmcli con add type team con-name team0 ifname team0 ip4 ${IPADDR} gw4 ${GWADDR} ipv4.dns '10.128.132.84 10.132.138.121 10.164.134.69' ipv4.method manual ipv6.method ignore
+    echo "TEAM_CONFIG='{\"runner\":{\"name\":\"activebackup\"},\"link_watch\":{\"name\":\"ethtool\"}}'" >> ${FDEVTEAM}
     cr_tm_ports
-  elif [[ ${MOD} -eq "LA" ]]; then
-    nmcli con add type bond con-name bond0 ifname bond0 ip4 ${IPADDR} GW4 ${GWADDR} ipv4.dns 'dns_addr1 dns_addr2 dns_addr3' ipv4.method manuall ipv6.method ignore
-    echo "BOND_OPTS=\"miimon=100 mode=802.3ad lacp_rate=1\"" >> ${FDEVBOND}
+  elif [ ${MOD} = "LA" ]; then
+    nmcli con add type bond con-name bond0 ifname bond0 ip4 ${IPADDR} gw4 ${GWADDR} ipv4.dns '10.128.132.84 10.132.138.121 10.164.134.69' ipv4.method manual ipv6.method ignore
+    sed -i '/BONDING_OPTS/d' ${FDEVBOND}
+    echo "BONDING_OPTS=\"miimon=100 mode=802.3ad lacp_rate=1\"" >> ${FDEVBOND}
     cr_bd_ports
   else
-     echo "It seems you do not understand what you are going to do. I'm thinking about it"
+     echo "It seems you do not understand what you are going to do. Think about it"
      exit 0
   fi
-elif [[ ${TYP} -eq "T" ]]; then
-  if [[ ${MOD} -eq "AB" ]]; then
-    nmcli con add type team con-name team0 ifname team0 ip4 ${IPADDDR} GW4 ${GWADDR} ipv4.dns 'dns_addr1 dns_addr2 dns_addr3' ipv4.method manual ipv6.method ignore
-    echo "TEAM_CONFIG='{\"funner\":{\"name\":\"activebackup\"},\"link_watch\":{\"name\":\"ethtool\"}}'" >> ${FDEVTEAM}
+elif [ ${TYP} = "T" ]; then
+  if [ ${MOD} = "AB" ]; then
+    nmcli con add type team con-name team0 ifname team0 ip4 ${IPADDR} gw4 ${GWADDR} ipv4.dns '10.128.132.84 10.132.138.121 10.164.134.69' ipv4.method manual ipv6.method ignore
+    echo "TEAM_CONFIG='{\"runner\":{\"name\":\"activebackup\"},\"link_watch\":{\"name\":\"ethtool\"}}'" >> ${FDEVTEAM}
     cr_tm_ports
-  elif [[ ${MOD} -eq "LA" ]]; then
-    nmcli con add type team con-name team0 ifname team0 ip4 ${IPADDDR} GW4 ${GWADDR} ipv4.dns 'dns_addr1 dns_addr2 dns_addr3' ipv4.method manual ipv6.method ignore
-    echo "TEAM_CONFIG='{\"funner\":{\"name\":\"lacp\",\"active\":true,\"fast_rate\":true,\"tx_hash\":[\"eth\",\"ipv4\",\"ipv6\"]},\"link_watch\":{\"name\":\"ethtool\"},\"ports\":{\"port1\":{},\"port2\":{}}}'" >> ${FDEVTEAM}
+  elif [ ${MOD} = "LA" ]; then
+    nmcli con add type team con-name team0 ifname team0 ip4 ${IPADDR} gw4 ${GWADDR} ipv4.dns '10.128.132.84 10.132.138.121 10.164.134.69' ipv4.method manual ipv6.method ignore
+    echo "TEAM_CONFIG='{\"runner\":{\"name\":\"lacp\",\"active\":true,\"fast_rate\":true,\"tx_hash\":[\"eth\",\"ipv4\",\"ipv6\"]},\"link_watch\":{\"name\":\"ethtool\"},\"ports\":{\"port1\":{},\"port2\":{}}}'" >> ${FDEVTEAM}
     cr_tm_ports
   else
-     echo "It seems you do not understand what you are going to do. I'm thinking about it"
+     echo "It seems you do not understand what you are going to do. Think about it"
      exit 0
   fi
 else
-  echo "It seems you do not understand what you are going to do. I'm thinking about it"
+  echo "It seems you do not understand what you are going to do. Think about it"
   exit 0
 fi
 
-rm -f ${TMPF1}
+rm -f ${FDEVTMP}
 
 systemctl restart network
 
